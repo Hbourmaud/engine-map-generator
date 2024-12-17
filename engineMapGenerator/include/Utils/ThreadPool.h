77063adbdf79bc1fce64972a8562fa3b66b97290
Hbourmaud <hugo.bourmaud@ynov.com>
2024-12-17 16:54:54 +0100

    feat: add simple Thread Pool

---
+++ engineMapGenerator/include/Utils/ThreadPool.h
@@ -0,0 +1,25 @@
+﻿#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+class ThreadPool
+{
+    public:
+        ThreadPool(size_t numThreads);
+        ~ThreadPool();
+
+        void enqueue(std::function<void()> task);
+
+    private:
+        std::vector<std::thread> threads;
+
+        std::queue<std::function<void()> > tasks;
+
+        std::mutex queueMutex;
+
+        std::condition_variable cv;
+    
+        bool stop = false;
+};
---
+++ engineMapGenerator/src/Utils/ThreadPool.cpp
@@ -0,0 +1,59 @@
+﻿#include "Utils/ThreadPool.h"
+
+ThreadPool::ThreadPool(size_t numThreads)
+{
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        threads.emplace_back([this] 
+        {
+            while (true)
+            {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock lock(queueMutex);
+
+                    cv.wait(lock, [this]
+                    {
+                        return !tasks.empty() || stop;
+                    });
+
+                    if (stop && tasks.empty())
+                    {
+                        return;
+                    }
+
+                    task = std::move(tasks.front());
+                    tasks.pop();
+                }
+
+                task();
+            }
+        });
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock lock(queueMutex);
+        stop = true;
+    }
+
+    cv.notify_all();
+
+    for(auto &thread : threads)
+    {
+        thread.join();
+    }
+}
+
+void ThreadPool::enqueue(std::function<void()> task)
+{
+    {
+        std::unique_lock lock(queueMutex);
+        tasks.emplace(std::move(task));
+    }
+
+    cv.notify_all();
+}
